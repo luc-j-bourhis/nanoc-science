00076195c1e1957fb372957a5b1a1052058533de
Luc J. Bourhis <luc_j_bourhis@mac.com>
2015-11-09 15:54:51 +0100

    Automatic generation of introduction about myself.

--- lib/default.rb
+++ lib/default.rb
@@ -2,3 +2,8 @@
 # before nanoc starts compiling.
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
+
+def language_of(item)
+    c = item.identifier.to_s.match(%r{/([a-z]{2})/})
+    c && c[1]
+end
