040ae853f69830ceb04b57c02ad67a13c3d99f95
Luc J. Bourhis <luc_j_bourhis@mac.com>
2016-07-29 10:02:23 +0900

    Implement a macro system

--- lib/default.rb
+++ lib/default.rb
@@ -7,3 +7,28 @@ def language_of(item)
     c = item.identifier.to_s.match(%r{/([a-z]{2})/})
     c && c[1]
 end
+
+class TextualMacros < Nanoc3::Filter
+  identifier :textual_macros
+  attr_accessor :macro
+
+  def initialize(arg)
+    super
+    @macro = {}
+    if not @item.nil? and not @item[:macros].nil?
+      @macro.merge!(@item[:macros])
+    end
+  end
+
+  def run(content, params={})
+    content.gsub(/%(\w+)/) do |m|
+      key = $1.intern
+      if not @macro[key].nil?
+        @macro[key]
+      else
+        m
+      end
+    end
+  end
+end
+
