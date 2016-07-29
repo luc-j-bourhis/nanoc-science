eb1023f56f27f9c8c9681cbf7d7e69805d71d6ce
Luc J. Bourhis <luc_j_bourhis@mac.com>
2016-07-29 10:03:56 +0900

    Mark user-chosen words as abbreviations
    Using <abbr> ... </abbr>

--- lib/default.rb
+++ lib/default.rb
@@ -32,3 +32,28 @@ class TextualMacros < Nanoc3::Filter
   end
 end
 
+class AbbreviationMarker < Nanoc3::Filter
+  identifier :mark_abbreviations
+  attr_accessor :abbr_rx
+
+  def initialize(arg)
+    super
+    @abbreviations = []
+    if not @item.nil? and not @item[:abbreviations].nil?
+      @abbreviations << @item[:abbreviations]
+    end
+    if not @abbreviations.empty?
+      @abbr_rx = /(#{@abbreviations.join("|")})/
+    else
+      @abbr_rx = nil
+    end
+  end
+
+  def run(content, params={})
+    if not @abbr_rx.nil?
+      content.gsub(@abbr_rx, '<abbr>\1</abbr>')
+    else
+      content
+    end
+  end
+end
