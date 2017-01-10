c52751ae093878440ea6214c2406bb596f4df59d
Luc J. Bourhis <luc_j_bourhis@mac.com>
2017-01-10 22:43:01 +0100

    Move to ruby 2.4, bundle update, remove nanoc-git (now coming with nanoc)

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 gem 'nanoc', '~> 4'
-gem 'nanoc-git' # Easy deploying to my GitHub pages
 gem 'adsf' # for nanoc view
 gem 'kramdown' # for Markdown
 gem 'nokogiri' # necessary for relative URL apparently
--- lib/default.rb
+++ lib/default.rb
@@ -4,7 +4,6 @@ require 'pp'
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering # to allow the use of `render` in layouts
-require 'nanoc-git'
 
 # Are we on the main page for one of the language?
 def main_page?
